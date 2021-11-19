aea63fd1f5dd5b7733b73a15a1d40a2b622b7808
filippo gallizia <galliziafilippo@gmail.com>
2021-11-19 12:14:00 +0000

    more

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,7 +16,7 @@ const corsOptions = {
 // app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use('/api', routes);
 
 db.sequelize
   .sync({ force: false })
