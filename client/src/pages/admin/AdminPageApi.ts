cd133108d1081ebe64bf96e851faeed21eda2e87
galliziafilippo@gmail.com <galliziafilippo@gmail.com>
2022-01-27 11:56:32 +0000

    docker client change to build

--- client/src/pages/admin/AdminPageApi.ts
+++ client/src/pages/admin/AdminPageApi.ts
@@ -1,7 +1,4 @@
-import axios from 'axios';
 import { BookingType } from '../../../types/Types';
-import { ACCESS_TOKEN } from '../../shared/locales/constant';
-import { ENDPOINT } from '../../api';
 import HttpService from '../../services/HttpService';
 
 export type BookingAndUsersResponse = {
@@ -31,45 +28,3 @@ class AdminPageApi {
 }
 
 export default new AdminPageApi();
-
-//export const getUsersAndBookings = async (fn: any) => {
-//  let config = {
-//    headers: {
-//      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
-//    },
-//  };
-//  try {
-//    const response = await axios.get(`${ENDPOINT}/usersAndBookings`, config);
-//    fn(response.data);
-//  } catch (e: any) {
-//    throw e;
-//  }
-//};
-
-//export const getAllUsers = async (fn: any) => {
-//  let config = {
-//    headers: {
-//      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
-//    },
-//  };
-//  try {
-//    const response = await axios.get(`${ENDPOINT}/allUsers`, config);
-//    fn(response.data);
-//  } catch (e: any) {
-//    throw e;
-//  }
-//};
-
-//export const getHolidays = async (fn: any) => {
-//  let config = {
-//    headers: {
-//      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
-//    },
-//  };
-//  try {
-//    const response = await axios.get(`${ENDPOINT}/getHolidays`, config);
-//    fn(response.data);
-//  } catch (e: any) {
-//    throw e;
-//  }
-//};
