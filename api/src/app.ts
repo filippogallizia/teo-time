1847aed54aa87b37cc339b037f1e65d0c7fbcc3e
galliziafilippo@gmail.com <galliziafilippo@gmail.com>
2022-06-05 12:29:03 +0200

    remove db sync

--- api/src/app.ts
+++ api/src/app.ts
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 db.sequelize
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     // chronJob to delete past bookings
     runEveryDay(db);
